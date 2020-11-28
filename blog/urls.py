7fa430d540f327320b5c08e603fc31f47453ef9e
NickBurnam <nickburnam789@gmail.com>
2020-11-28 12:39:10 -0800

    Categories

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 import mainApp.views
-from .views import BlogView, BlogPostView, AddBlogPostView, EditPostView, DeletePostView
+from .views import BlogView, BlogPostView, AddBlogPostView, EditPostView, DeletePostView, CategoryView
 
 app_name = "blog"
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('addPost/', AddBlogPostView.as_view(), name="add_post"),
     path('article/edit/<int:pk>', EditPostView.as_view(), name="edit_post"),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
+    path('category/<str:cat>', CategoryView, name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,50 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import BlogArticle 
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from .models import BlogArticle
 from django.urls import reverse_lazy
 
 # Create your views here.
-#def blogIndex(request):
+# def blogIndex(request):
 #     return render(request, 'blogIndex.html')
 
+
 class BlogView(ListView):
-     model = BlogArticle
-     template_name = 'blog/blogIndex.html'
-     ordering = ['-date']
+    model = BlogArticle
+    template_name = "blog/blogIndex.html"
+    ordering = ["-date"]
+
 
 class BlogPostView(DetailView):
-     model = BlogArticle
-     template_name = 'blog/blogPost.html'
+    model = BlogArticle
+    template_name = "blog/blogPost.html"
+
 
 class AddBlogPostView(CreateView):
-     model = BlogArticle
-     template_name = 'blog/addBlogPost.html'
-     fields = '__all__'
+    model = BlogArticle
+    template_name = "blog/addBlogPost.html"
+    fields = "__all__"
+
 
 class EditPostView(UpdateView):
-     model = BlogArticle
-     template_name = 'blog/editBlogPost.html'
-     fields = '__all__'
+    model = BlogArticle
+    template_name = "blog/editBlogPost.html"
+    fields = "__all__"
+
 
 class DeletePostView(DeleteView):
-     model = BlogArticle
-     template_name = 'blog/deleteBlogPost.html' 
-     success_url = reverse_lazy('blog:blog')
+    model = BlogArticle
+    template_name = "blog/deleteBlogPost.html"
+    success_url = reverse_lazy("blog:blog")
+
+
+def CategoryView(request, cat):
+    category_post = BlogArticle.objects.filter(category=cat)
+    return render(
+        request, "blog/categories.html", {"cat": cat, "category_post": category_post}
+    )
